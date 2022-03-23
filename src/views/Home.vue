f2c19843cab8f95c71a403a5a5760741aada9019
jiangvicc <binbinbb520@gmail.com>
2022-03-23 22:40:46 +0800

    fix Bonus style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -266,18 +266,18 @@
   </section>
   <div class="bg-soft text-white py-2 py-md-5">
   <div class="container px-6 px-md-9">
-    <ul class="row list-unstyled gx-md-9">
-      <li class="col-md-4 mb-5 mb-md-0 py-2">
-        <div class="d-flex d-md-column flex-md-column flex-lg-row align-items-center justify-content-center justify-content-lg-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="0">
+    <ul class="row list-unstyled mb-0 gx-md-9">
+      <li class="col-md-4 mb-0 mb-md-0 py-2">
+        <div class="d-flex d-md-column flex-md-column flex-lg-row align-items-center justify-content-start justify-content-md-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="0">
           <i class="bi bi-cart3 mb-md-3 mb-lg-0 display-5"></i>
           <div class="ms-4 ms-md-0 ms-lg-4 text-md-center text-lg-start">
             <h3 class="h5">Free</h3>
-            <p class="mb-0">所有會員滿 $1000 免運</p>
+            <p class="mb-0">所有會員立即享免運</p>
           </div>
         </div>
       </li>
-      <li class="col-md-4 mb-5 mb-md-0 py-2">
-        <div class="d-flex d-md-column flex-md-column flex-lg-row align-items-center justify-content-center  justify-content-lg-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="0">
+      <li class="col-md-4 mb-0 mb-md-0 py-2">
+        <div class="d-flex d-md-column flex-md-column flex-lg-row align-items-center justify-content-start  justify-content-md-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="0">
            <i class="bi bi-gift mb-md-3 mb-lg-0 display-5"></i>
           <div class="ms-4 ms-md-0 ms-lg-4 text-md-center text-lg-start">
             <h3 class="h5">Bonus</h3>
@@ -285,11 +285,11 @@
           </div>
         </div>
       </li>
-      <li class="col-md-4 mb-5 mb-md-0 py-2">
-        <div class="d-flex d-md-column flex-md-column flex-lg-row align-items-center justify-content-center  justify-content-lg-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="0">
+      <li class="col-md-4 mb-0 mb-md-0 py-2">
+        <div class="d-flex d-md-column flex-md-column flex-lg-row align-items-center justify-content-start  justify-content-md-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="0">
            <i class="bi bi-credit-card mb-md-3 mb-lg-0 display-5"></i>
           <div class="ms-4 ms-md-0 ms-lg-4 text-md-center text-lg-start">
-            <h3 class="h5 text-nowrap">Payment Method</h3>
+            <h3 class="h5 text-nowrap">Payment</h3>
             <p class="mb-0">多元支付方式</p>
           </div>
         </div>
@@ -315,7 +315,7 @@
     </div>
     <!-- </div> -->
   </div>
-  <div id="plus"><span>新品上市</span></div>
+  <div class="" id="plus"><span >新品上市</span></div>
 </div>
 <!-- GoTop -->
 <div class="footer">
