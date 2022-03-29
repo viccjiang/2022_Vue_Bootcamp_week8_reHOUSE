4bc6e6664cb9289cd5bf27ef02ae867500b6c5aa
jiangvicc <binbinbb520@gmail.com>
2022-03-29 14:31:49 +0800

    fix back nav color

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-soft">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">re.HOUSE</a>
       <button
